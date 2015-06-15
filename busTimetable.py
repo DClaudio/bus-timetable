89201377e86f38b87294d6ca5df79a74839dc568
DClaudio <david_claudio_t@yahoo.com>
2015-06-15 16:56:26 +0100

    added LCD control example

--- busTimetable.py
+++ busTimetable.py
@@ -1,10 +1,16 @@
 from urllib2 import Request, urlopen, URLError
+import mraa
 
-request = Request('http://placekitten.com/')
-
-try:
-	response = urlopen(request)
-	kittens = response.read()
-	print kittens[559:1000]
-except URLError, e:
+def httpReqestExample(*args):
+	request = Request('http://placekitten.com/')
+	try:
+		response = urlopen(request)
+		kittens = response.read()
+		print kittens[559:1000]
+	except URLError, e:
     print 'No kittez. Got an error code:', e
+
+def control_lcd(*args):
+	x = mraa.Gpio(13)
+	x.dir(mraa.DIR_OUT)
+	x.write(1)
