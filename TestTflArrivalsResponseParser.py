4221faedf525c4f70d6b3ce1e0ab4f302ecb6606
DClaudio <david_claudio_t@yahoo.com>
2015-06-17 16:31:24 +0100

    completed json parsing part

--- TestTflArrivalsResponseParser.py
+++ TestTflArrivalsResponseParser.py
@@ -1,15 +1,17 @@
 import unittest
+import json
 from TflArrivalsResponseParser import *
 
 class TestTflArrivalsResponseParser(unittest.TestCase):
 
 	def test_parse_response(self):
-		eample_file = open('example_response.json')
-		response_string = eample_file.read()
-		expected_result = {'name': "test", 'number': 1}
+		eample_response_file = open('example_response.json')
+		response_string = eample_response_file.read()
+		expected_result_file = open('expected_result.json')
+		expected_result = json.loads(expected_result_file.read())
 
 		responseParser = TflArrivalsResponseParser()		
-		self.assertEqual(responseParser.parse_response(response_string), expected_result)
+		self.assertEqual(responseParser.get_formated_response(response_string), expected_result)
 
 
 unittest.main()
--- TflArrivalsResponseParser.py
+++ TflArrivalsResponseParser.py
@@ -5,6 +5,15 @@ class TflArrivalsResponseParser:
 	def parse_response(self, http_response):
 		return json.loads(http_response)
 
-	def remove_unused_attr(self, data):
-		
+	def get_formated_response(self, http_response):
+		response = self.parse_response(http_response)
+		filtered_response = map(self.filter_dictionary,response)
+		sorted_response = sorted(filtered_response, key = lambda k: k['timeToStation'])
+		return sorted_response
 
+	def filter_dictionary(self, entry):
+		return {
+				'route': entry['lineName'],
+				'destination': entry['destinationName'],
+				'timeToStation': entry['timeToStation']
+				}
