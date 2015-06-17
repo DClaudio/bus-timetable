fdeff70f5b1f673adad972fd956551e3d1f0bcd6
DClaudio <david_claudio_t@yahoo.com>
2015-06-17 14:43:27 +0100

    modified url builder test

--- TestTflApiUrlBuilder.py
+++ TestTflApiUrlBuilder.py
@@ -7,7 +7,8 @@ class TestTflApiUrlBuilder(unittest.TestCase):
 		action_category = 'StopPoint'
 		action = 'Arrivals'
 		station_id = '490012375E'
-		expected_url = 'https://api.tfl.gov.uk/StopPoint/Arrivals?ids=490012375E&app_id=7df32329&app_key=b13d2099c8c5b37c0cbbe29c7b37faae'
+		expected_url = 'https://api.tfl.gov.uk/StopPoint/Arrivals?ids=490012375E' \
+						+'&app_id=7df32329&app_key=b13d2099c8c5b37c0cbbe29c7b37faae'
 
 		url_builder = TflApiUrlBuilder(action_category)
 		url = url_builder.with_action(action).with_station_id(station_id).build_url()
