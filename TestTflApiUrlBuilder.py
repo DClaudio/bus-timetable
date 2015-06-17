627bded4998d8a8cc141b0dbc86703d83c046d04
DClaudio <david_claudio_t@yahoo.com>
2015-06-17 17:22:07 +0100

    refined unit test to reflect the way tfl api is used

--- TestTflApiUrlBuilder.py
+++ TestTflApiUrlBuilder.py
@@ -5,9 +5,9 @@ class TestTflApiUrlBuilder(unittest.TestCase):
 
 	def test_url_builder(self):
 		action_category = 'StopPoint'
-		action = 'Arrivals'
+		action = '%7Bids%7D/Arrivals'
 		station_id = '490012375E'
-		expected_url = 'https://api.tfl.gov.uk/StopPoint/Arrivals?ids=490012375E' \
+		expected_url = 'https://api.tfl.gov.uk/StopPoint/%7Bids%7D/Arrivals?ids=490012375E' \
 						+'&app_id=7df32329&app_key=b13d2099c8c5b37c0cbbe29c7b37faae'
 
 		url_builder = TflApiUrlBuilder(action_category)
