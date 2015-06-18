dcc40e264b439c7a9017a3d7b57be059117cd718
DClaudio <david_claudio_t@yahoo.com>
2015-06-18 10:38:52 +0100

    added resources folder

--- TestTflArrivalsResponseParser.py
+++ TestTflArrivalsResponseParser.py
@@ -5,10 +5,12 @@ from TflArrivalsResponseParser import *
 class TestTflArrivalsResponseParser(unittest.TestCase):
 
 	def test_parse_response(self):
-		eample_response_file = open('example_response.json')
+		eample_response_file = open('resources/example_response.json')
 		response_string = eample_response_file.read()
-		expected_result_file = open('expected_result.json')
+		eample_response_file.close()
+		expected_result_file = open('resources/expected_result.json')
 		expected_result = json.loads(expected_result_file.read())
+		expected_result_file.close()
 
 		responseParser = TflArrivalsResponseParser()		
 		self.assertEqual(responseParser.get_formated_response(response_string), expected_result)
