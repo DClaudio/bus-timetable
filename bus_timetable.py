e5cb9980dfb9a8af8df05ab52e8c98ff1c48573f
DClaudio <david_claudio_t@yahoo.com>
2015-10-14 15:34:33 +0100

    MVP for bus timetable

--- bus_timetable.py
+++ bus_timetable.py
@@ -2,6 +2,8 @@ from urllib2 import Request, urlopen, URLError
 
 from TflArrivalsResponseParser import *
 from TflApiUrlBuilder import *
+from GrooveLCD.py import *
+import time
 
 def requestDataFromTfl(*args):
 	responseParser = TflArrivalsResponseParser() 
@@ -10,9 +12,11 @@ def requestDataFromTfl(*args):
 		response = urlopen(request)
 		arrivals_data = response.read()
 		formated_data = responseParser.get_formated_response(arrivals_data)
-		print_arrivals_console(formated_data)
+		#print_arrivals_console(formated_data)
+		return formated_data
 	except URLError, e:
 		print 'Request Error: ', e
+		return ['Error: data', 'fetch FAILED']
 
 def print_arrivals_console(data):
 	print '  Bus  Time   Destination'
@@ -21,5 +25,11 @@ def print_arrivals_console(data):
 			i['destination'], w0=3, w1=5, w2=17)
 
 
+def infiniteRun(*args):
+	mylcd = init_lcd
+	while(true):
+		data = requestDataFromTfl()
+		print_on_lcd(mylcd, data[0], data[1])
+		sleep(30)
 
-requestDataFromTfl()
+infiniteRun()
